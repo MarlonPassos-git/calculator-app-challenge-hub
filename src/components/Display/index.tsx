f43af00e7fa80c329d028a4ff9613dc40c3e2ad1
marlon Felipe Passos <1marlonpassos@gmail.com>
2022-01-06 20:24:37 -0300

    create display component

---
+++ src/components/Display/index.tsx
@@ -0,0 +1,11 @@
+import { Container, Equation } from "./display-style";
+
+export function Display() { 
+    return (
+        <Container>
+            <Equation>
+                399,981
+            </Equation>
+        </Container>
+    )
+}
