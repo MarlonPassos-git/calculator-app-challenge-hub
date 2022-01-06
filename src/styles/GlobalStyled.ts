fd3a5edf7626a4c513ff4685aa8a5dce408b6f28
marlon Felipe Passos <1marlonpassos@gmail.com>
2022-01-06 19:41:52 -0300

    add style to center component to main

--- src/styles/GlobalStyled.ts
+++ src/styles/GlobalStyled.ts
@@ -5,18 +5,28 @@ export const GlobalStyle = createGlobalStyle`
 
     :root {
         // Fonts
-        --font-extra-large: ${progressiveClamp(32, 48)} 
-        --font-large: ${Rem(32)}
-        --font-medium: ${progressiveClamp(28, 32)}
-        --font-small: ${progressiveClamp(16, 20)}
-        --font-extra-small: ${Rem(12)}
+        --font-extra-large: ${progressiveClamp(32, 48)}; 
+        --font-large: ${Rem(32)};
+        --font-medium: ${progressiveClamp(28, 32)};
+        --font-small: ${progressiveClamp(16, 20)};
+        --font-extra-small: ${Rem(12)};
     }
 
 
     body {
-        background: red;
+        background-color: ${ props => props.theme.colors.background };
         font-family: 'Spartan', sans-serif;
         font-weight: bold;
+
+        display: grid;
+        place-items: center;
+        min-height: 100vh;
+    }
+
+    #root {
+        width: fill-available;
+        max-width: ${Rem(540)};
+        margin: ${Rem(28)} ${Rem(24)} ${Rem(31)};
     }
 
     button {
