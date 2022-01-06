c498a01feb1a43e5c2de7dd839c43e4883252e62
marlon Felipe Passos <1marlonpassos@gmail.com>
2022-01-06 20:24:58 -0300

    create display style

---
+++ src/components/Display/display-style.ts
@@ -0,0 +1,21 @@
+import styled from 'styled-components'
+import { progressiveClamp, Rem } from 'progressive-size'
+
+export const Container = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: flex-end;
+
+    height: ${progressiveClamp(88, 118)};
+    width: 100%;
+    margin: ${Rem(32)} 0 ${Rem(24)};
+    border-radius: ${Rem(10)}; 
+    padding: 0 ${progressiveClamp(24, 32)};
+
+    background: ${props => props.theme.colors.display.background};
+`
+
+export const Equation = styled.span`
+    font-size: var(--font-extra-large);
+    color: ${props => props.theme.colors.display.text};
+`
