f4af7c74065b0cfcf0f701a18ec2a03692869fa6
marlon Felipe Passos <1marlonpassos@gmail.com>
2022-01-05 21:17:32 -0300

    add variables fonts

--- src/styles/GlobalStyled.ts
+++ src/styles/GlobalStyled.ts
@@ -1,7 +1,17 @@
-import { createGlobalStyle  }from 'styled-components'
-
+import { createGlobalStyle  } from 'styled-components'
+import { progressiveClamp, Rem } from 'progressive-size'
 
 export const GlobalStyle = createGlobalStyle`
+
+    :root {
+        // Fonts
+        --font-extra-large: ${progressiveClamp(32, 48)} 
+        --font-large: ${Rem(32)}
+        --font-medium: ${progressiveClamp(28, 32)}
+        --font-small: ${progressiveClamp(16, 20)}
+        --font-extra-small: ${Rem(12)}
+    }
+
 
     body {
         background: red;
