bdf94b5b718c7b2063561e39130a64cecebf87e4
marlon Felipe Passos <1marlonpassos@gmail.com>
2022-01-06 22:25:54 -0300

    create styte key

---
+++ src/components/Key/key-style.ts
@@ -0,0 +1,39 @@
+import styled from 'styled-components'
+import { progressiveClamp } from 'progressive-size'
+
+
+interface ContainerProps { 
+    size?: '1' | '2',
+    color: 'equal' | 'wipers' | 'default'
+}
+
+export const Container = styled.button<ContainerProps>`
+    grid-column: ${props => (props.size === '1' || props.size === undefined) ? 'span 1' : 'span 2'};
+    
+    width: 100%;
+    padding-top: 3px;
+    border-radius: ${progressiveClamp(5, 10)};
+    
+    text-transform: uppercase;
+    text-align: center;
+    font-size: ${(props) => { 
+        if (props.color === 'equal' || props.color === 'wipers') {
+            return 'var(--font-small)'
+        }
+        return 'var(--font-medium)'
+    
+    }};
+    color: ${props =>  props.theme.colors.button.text[props.color] };
+        
+    background-color: ${props =>  props.theme.colors.button.background[props.color].normal };
+    box-shadow: 
+        0px -4px 0px 0px 
+        ${props => props.theme.colors.button.background[props.color].shadow}
+        inset
+    ;
+
+    &:hover {
+        background-color: ${props =>  props.theme.colors.button.background[props.color].hover };
+    }
+    
+`
