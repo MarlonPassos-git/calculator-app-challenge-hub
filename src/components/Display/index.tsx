56aeeee955bf6672c85b64341bd05b992e4dfb33
marlon Felipe Passos <1marlonpassos@gmail.com>
2022-01-07 20:15:25 -0300

    add pattern to add text in display

--- src/components/Display/index.tsx
+++ src/components/Display/index.tsx
@@ -1,11 +1,27 @@
+import { useState } from "react";
+import { useDados } from "../../context/context";
 import { Container, Equation } from "./display-style";
 
-export function Display() { 
+export function Display() {
+    // @ts-ignore
+    const { equation, setEquation } = useDados();
+
+    function handleInput(e: React.ChangeEvent<HTMLInputElement>) { 
+        const regex = /^([0-9]|\+|\.|-|x|\/|\*)*$/
+
+        if (regex.test(e.target.value)) { 
+            setEquation(e.target.value)
+        }
+    }
+
     return (
         <Container>
-            <Equation>
-                399,981
-            </Equation>
+            <Equation
+                value={equation}
+                type="text"
+                onChange={handleInput}
+                pattern="/[0-9]*/"
+            />
         </Container>
     )
 }
