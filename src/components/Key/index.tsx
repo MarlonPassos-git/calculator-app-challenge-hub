89b9d0bfc03896c29003c220cb3b7883e535c665
marlon Felipe Passos <1marlonpassos@gmail.com>
2022-01-06 22:25:32 -0300

    create component key

---
+++ src/components/Key/index.tsx
@@ -0,0 +1,49 @@
+import { Container } from "./key-style";
+
+interface KeyProps { 
+    children: string;
+}
+
+export function Key({children}: KeyProps) { 
+
+    const differentStyles = {
+        'del': {
+            size: '1',
+            color: 'wipers'
+        },
+        '=': {
+            size: '2',
+            color: 'equal'
+        },
+        'reset': {
+            size: '2',
+            color: 'wipers'
+        },
+    }
+
+    if (differentStyles.hasOwnProperty(children)) {  
+        console.log(children);
+        return (
+            <Container
+                size={
+                    //@ts-ignore    
+                    differentStyles[children].size
+                }
+                color={ 
+                    //@ts-ignore
+                    differentStyles[children].color
+                }
+            >
+                { children }
+            </Container>
+        )
+    }    
+
+    return (
+        <Container
+            color={'default'}
+        >
+                { children }
+        </Container>
+    )
+}
