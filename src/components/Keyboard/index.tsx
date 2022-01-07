eb7a8d912bfb7db8ac72996fbc59d970af276f70
marlon Felipe Passos <1marlonpassos@gmail.com>
2022-01-06 22:26:08 -0300

    create keyboard component

---
+++ src/components/Keyboard/index.tsx
@@ -0,0 +1,18 @@
+import { Container } from "./keyboard-style";
+import { Key } from '../Key';
+
+export function Keyboard() { 
+    const keys = [
+        '7', '8', '9', 'del',
+        '4', '5', '6', '+',
+        '1', '2', '3', '-',
+        '.', '0', '/', 'x',
+        'reset', '='
+    ];
+
+    return (
+        <Container>
+            { keys.map(key => <Key key={key}>{key}</Key>) }
+        </Container>
+    )
+}
