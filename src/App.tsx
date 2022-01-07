c788d5b30b4561cb946ddbc5d91123e82c91b1e7
marlon Felipe Passos <1marlonpassos@gmail.com>
2022-01-06 22:24:44 -0300

    add kyboard to page

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,20 @@ import React from 'react';
 import "the-new-css-reset/css/reset.css"
 import { GlobalStyle } from './styles/GlobalStyled';
 import { ThemeProvider } from 'styled-components'
-import { theme1 } from './styles/Theme'
 import { Display } from './components/Display';
 import { Header } from './components/Header';
+import { Keyboard } from './components/Keyboard';
+import { theme3 } from './styles/Theme'
+
 
 function App(){
   return (
-      <ThemeProvider theme={theme1} >
+      <ThemeProvider theme={theme3} >
         <Header/>
         <Display />
+        <Keyboard />
         <GlobalStyle />
+
     </ThemeProvider>
       
     
