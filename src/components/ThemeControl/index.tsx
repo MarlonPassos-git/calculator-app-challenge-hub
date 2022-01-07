68cffa8a854cacef79b13b89047d8f35a43d62b0
marlon Felipe Passos <1marlonpassos@gmail.com>
2022-01-06 23:28:47 -0300

    change theme to click the number button

--- src/components/ThemeControl/index.tsx
+++ src/components/ThemeControl/index.tsx
@@ -20,15 +20,28 @@ export function ThemeControl({ theme, setTheme }: ThemeControlProps) {
                 Theme
             </Name>
             <WrapperButtons>
-                <Button>1</Button>
-                <Button>2</Button>
-                <Button>3</Button>
+                <Button
+                    onClick={() => setTheme(1)}
+                >
+                    1
+                </Button>
+                <Button
+                    onClick={() => setTheme(2)}
+                >
+                    2
+                </Button>
+                <Button
+                    onClick={() => setTheme(3)}
+                >
+                    3
+                </Button>
             </WrapperButtons>
             <Range
                 onChange={(e) => handledRange(e.target)}
                 type="range"
                 min="1"
                 max="3"
+                value={theme}
             />
         </Container>
     ) 
