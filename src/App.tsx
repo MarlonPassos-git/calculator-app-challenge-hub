0026f98bc38e2b89487dea33924fa52fcc88b8eb
marlon Felipe Passos <1marlonpassos@gmail.com>
2022-01-07 22:44:21 -0300

    add shortcut to keyboar to calculate

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ import { Keyboard } from './components/Keyboard';
 import { theme1 } from './styles/Theme'
 import { DadosProvider } from './context/context';
 
+
 interface ThemeProps { 
   colors: {
     background: string,
@@ -56,8 +57,9 @@ function App() {
   const [themeNumber, setThemeNumber] = useState(1);
   const [theme, setTheme] = useState<ThemeProps>(theme1);
 
+  
+
   useEffect(() => { 
-    console.log(localStorage.getItem('themeNumber'))
 
     if (localStorage.getItem('themeNumber') === null) {
       localStorage.setItem('themeNumber', '1');
@@ -67,32 +69,55 @@ function App() {
       setThemeNumber(+localStorage.getItem('themeNumber'));
     }
 
+    window.addEventListener('keydown', (e: KeyboardEvent) => { 
+      const keys = document.querySelectorAll('[data-value]')
+      const keysValue: (string|null)[] = [] 
+      const exception = {
+        del: 'Backspace',
+        '=': 'Enter',
+        'reset': 'r',
+      }
+
+      keys.forEach((key) => { 
+        const keyValue = key.getAttribute('data-value');
+
+        // @ts-ignore
+        if (exception[keyValue]) {
+          // @ts-ignore
+            keysValue.push(exception[keyValue])
+        }
+        else {
+          keysValue.push(keyValue)
+        }
+
+      })
+
+      if (keysValue.includes(e.key)) { 
+        const position = keysValue.indexOf(e.key);
+        // @ts-ignore
+        keys[position].click();
+      }
+    })
+
     
   }, [])
   
   useEffect(() => {
     
-    console.log('carreguei no iniciar ')
+    
 
     if (themeNumber === 1) { 
-  
       import('./styles/themes/theme1').then((theme) => { 
-            
-        console.log('1')
         setTheme(theme.default);
         })
     }
     if (themeNumber === 2) {
       import('./styles/themes/theme2').then((theme) => { 
-            
-        console.log('2')
         setTheme(theme.default);
         })
     }
     if (themeNumber === 3) {
       import('./styles/themes/theme3').then((theme) => { 
-            
-        console.log('3')
         setTheme(theme.default);
         })
     }
@@ -110,9 +135,6 @@ function App() {
         <GlobalStyle />
       </ThemeProvider>
     </DadosProvider>
-      
-      
-    
   );
 }
 
