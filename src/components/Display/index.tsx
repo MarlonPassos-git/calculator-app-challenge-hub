c613e16aedb53139d9c2de0de9fa7e3492771e85
marlon Felipe Passos <1marlonpassos@gmail.com>
2022-01-07 21:13:38 -0300

    save last equation to local storage

--- src/components/Display/index.tsx
+++ src/components/Display/index.tsx
@@ -1,10 +1,29 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useDados } from "../../context/context";
 import { Container, Equation } from "./display-style";
 
 export function Display() {
     // @ts-ignore
     const { equation, setEquation } = useDados();
+
+    useEffect(() => { 
+
+    if (localStorage.getItem('historicEquation') !== null) {
+        setEquation(localStorage.getItem('historicEquation'));
+    }
+    
+  }, [])
+
+    useEffect(() => { 
+        const regex = /(\+|-|\.|x|\/){2}/i;
+        
+        if (regex.test(equation)) { 
+            setEquation(equation.slice(0, -1))
+        }
+
+        localStorage.setItem('historicEquation', equation.toString());
+
+    }, [equation]);
 
     function handleInput(e: React.ChangeEvent<HTMLInputElement>) { 
         const regex = /^([0-9]|\+|\.|-|x|\/|\*)*$/
